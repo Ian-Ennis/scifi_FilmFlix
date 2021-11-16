c4c92f3d719de12c1efbd2cb07330cf4cdb3ba91
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2021-11-16 10:39:03 -0700

    merged fetch request and card maker

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-// How do you specify parameters in a URL?
-// Any word after the question mark (?) in a URL is considered to be a parameter which can hold values. The value for the corresponding parameter is given after the symbol “equals” (=). Multiple parameters can be passed through the URL by separating them with multiple “&”.
+// API key is apikey=582a980c&
+// oMDb url with title and year parameters"http://www.omdbapi.com/?apikey=582a980c&t=dune&y=2021";
 
-// How do I add parameters to a URL query?
-// Query parameters are a defined set of parameters attached to the end of a url. They are extensions of the URL that are used to help define specific content or actions based on the data being passed. To append query params to the end of a URL, a ‘? ‘ Is added followed immediately by a query parameter.
+// form variables
+const form = document
+  .getElementById("form")
+  .addEventListener("submit", handleSubmit);
 
-const oMDbAPI = ('http://www.omdbapi.com/?apikey=582a980c&t=dune&y=2021')
-// http://www.omdbapi.com/?apikey=582a980c&t=dune&y=2021
+// submit event for user input, including fetch
+function handleSubmit(e) {
+  e.preventDefault();
+  const movieInput = e.target.movieName.value;
+  const yearInput = e.target.movieYear.value;
+  const oMDbAPI = `http://www.omdbapi.com/?apikey=582a980c&t=${movieInput}&y=${yearInput}`
 
-
-fetch (oMDbAPI)
+  fetch(oMDbAPI)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => renderCard(data))
+}
+
+// fetch utilization to generate card
+function renderCard(data) {
+    console.log(data)
+    const poster = document.getElementById('moviePoster').src = data.Poster;
+    const synopsis = document.getElementById('synopsis').innerText = data.Plot;
+    const year = document.getElementById('year').innerText = data.Year;
+    const cast = document.getElementById('cast').innerText = data.Actors;
+    const director = document.getElementById('director').innerText = data.Director;
+}
 
