e90d567a0a3ecd7aed19fd817a2742a73250acb3
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2021-11-16 19:50:05 -0700

    dual-source API utilization

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
-// form variables
-const form = document
-  .getElementById("form")
-  .addEventListener("submit", handleSubmit);
+// `${tMDBPoster}${imagePath}`
 
-// submit event for user input, including fetch
+const form = document.getElementById("form").addEventListener("submit", handleSubmit);
+
+
 function handleSubmit(e) {
   e.preventDefault();
+
   const movieInput = e.target.movieName.value;
   const yearInput = e.target.movieYear.value;
+
+  const tMDB = `http://api.themoviedb.org/3/search/movie?query=${movieInput}&api_key=c0d223e63c9747fda47653e647837001`;
+
+  fetch(tMDB)
+    .then((res) => res.json())
+    .then((tMDBdata) => renderPlotRelease(tMDBdata));
+
+
   const oMDbAPI = `http://www.omdbapi.com/?apikey=582a980c&t=${movieInput}&y=${yearInput}`
 
   fetch(oMDbAPI)
-    .then(res => res.json())
-    .then(data => renderCard(data))
+    .then(resp => resp.json())
+    .then(oMDBdata => renderDirectorCast(oMDBdata))
 }
 
-// fetch utilization to generate card
-function renderCard(data) {
-    console.log(data)
-    const poster = document.getElementById('moviePoster').src = data.Poster;
-    const synopsis = document.getElementById('synopsis').innerText = data.Plot;
-    const year = document.getElementById('year').innerText = data.Year;
-    const cast = document.getElementById('cast').innerText = data.Actors;
-    const director = document.getElementById('director').innerText = data.Director;
+
+function renderPlotRelease(tMDBdata) {
+  console.log(tMDBdata)
+  // const tMDBPoster = `https://image.tmdb.org/t/p/w185`;
+  // const imagePath = tMDBdata.results[0].poster_path;
+  // const poster = document.getElementById('moviePoster').src = `${tMDBPoster}${imagePath}`;
+
+  const synopsis = document.getElementById('plot').innerText = tMDBdata.results[0].overview;
+  const year = document.getElementById('releaseDate').innerText = `Release Date: ${tMDBdata.results[0].release_date}`;
+}
+
+function renderDirectorCast(oMDBdata) {
+  console.log(oMDBdata);
+
+  const cast = document.getElementById('cast').innerText = `Cast: ${oMDBdata.Actors}`
+  const director = document.getElementById('director').innerText = `Director(s): ${oMDBdata.Director}`
 }
 
