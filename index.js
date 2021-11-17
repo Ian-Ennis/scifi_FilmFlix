5b6a3dae2c2d649070053697d49842640ef80021
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2021-11-16 20:57:02 -0700

    attempt to hide undefined stats

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function handleSubmit(e) {
 
   fetch(tMDB)
     .then((res) => res.json())
-    .then((tMDBdata) => renderPlotRelease(tMDBdata));
+    .then((tMDBdata) => renderPicPlotRelease(tMDBdata));
 
 
   const oMDbAPI = `http://www.omdbapi.com/?apikey=582a980c&t=${movieInput}&y=${yearInput}`
@@ -24,11 +24,11 @@ function handleSubmit(e) {
 }
 
 
-function renderPlotRelease(tMDBdata) {
+function renderPicPlotRelease(tMDBdata) {
   console.log(tMDBdata)
-  // const tMDBPoster = `https://image.tmdb.org/t/p/w185`;
-  // const imagePath = tMDBdata.results[0].poster_path;
-  // const poster = document.getElementById('moviePoster').src = `${tMDBPoster}${imagePath}`;
+  const tMDBPoster = `https://image.tmdb.org/t/p/w185`;
+  const imagePath = tMDBdata.results[0].poster_path;
+  const poster = document.getElementById('moviePoster').src = `${tMDBPoster}${imagePath}`;
 
   const synopsis = document.getElementById('plot').innerText = tMDBdata.results[0].overview;
   const year = document.getElementById('releaseDate').innerText = `Release Date: ${tMDBdata.results[0].release_date}`;
@@ -37,7 +37,29 @@ function renderPlotRelease(tMDBdata) {
 function renderDirectorCast(oMDBdata) {
   console.log(oMDBdata);
 
-  const cast = document.getElementById('cast').innerText = `Cast: ${oMDBdata.Actors}`
-  const director = document.getElementById('director').innerText = `Director(s): ${oMDBdata.Director}`
+  let cast = document.getElementById('cast').innerText = `Cast: ${oMDBdata.Actors}`
+  let director = document.getElementById('director').innerText = `Director(s): ${oMDBdata.Director}`
+
+  if (cast === `Cast: undefined`) {
+    cast.style.display = "none";
+  }
+
+  if (director === `Director(s): undefined`) {
+    director.style.display = "none";
+  }
 }
 
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const addBtn = document.querySelector("#new-toy-btn");
+//   const toyFormContainer = document.querySelector(".container");
+//   addBtn.addEventListener("click", () => {
+//     // hide & seek with the form
+//     addToy = !addToy;
+//     if (addToy) {
+//       toyFormContainer.style.display = "block";
+//     } else {
+//       toyFormContainer.style.display = "none";
+//     }
+//   });
+// });
